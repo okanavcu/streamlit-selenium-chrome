10dc5f9254aa0c668bb76bf32648140af5d30308
Okan Avcu <134762733+okanavcu@users.noreply.github.com>
2023-11-19 20:42:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,22 @@
-from datetime import datetime
 import streamlit as st
-from streamlit_extras.grid import grid
-import pandas as pd
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 
-st.set_page_config(layout='wide',initial_sidebar_state='collapsed') 
-def main():
-    my_grid = grid([10,1,1], vertical_align="top")
-    my_grid.title("6661 AGİ DESTEK KONTROL ROBOTU")
-    aylar = {"01": "Ocak", "02": "Şubat", "03": "Mart", "04": "Nisan", "05": "Mayıs", "06": "Haziran",
-            "07": "Temmuz", "08": "Ağustos", "09": "Eylül", "10": "Ekim", "11": "Kasım", "12": "Aralık"}
-    selected_month = my_grid.selectbox("Ay Seç", aylar.values())
-    selected_year = my_grid.number_input("Yıl Seç", min_value=2000, max_value=datetime.now().year, step=1, value=datetime.now().year)
-    selected_month_key = [key for key, value in aylar.items() if value == selected_month][0]
-    st.success("Seçilen Dönem: " + str(selected_month_key) + "/" + str(selected_year))
-    df = pd.DataFrame([
-        {"SAP Kodu": "", "İŞ YERİ ADI": "", "KULLANICI ADI": "", "KULLANICI KODU": "", "SİSTEM ŞİFRESİ": "","İŞYERİ ŞİFRESİ": "",str(selected_month) + "-" + str(selected_year)+ " 6661 AGİ Destek Tutarları": "", }])
-    
-    edit_df = st.data_editor(df,use_container_width=True, num_rows="dynamic",)
-    if st.button("Robot Başlat",key="start",):
-        pass
-    if st.button("Raporu İndir",key="download"):
-        edit_df.to_excel(str(selected_month) + "-" + str(selected_year)+ " 6661 AGİ Destek Tutarları Raporu.xlsx", index=False)
-        st.toast(body="Excel dosyası indirildi!",)
+@st.experimental_singleton
+def get_driver():
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-if __name__ == "__main__":
-    main()
+options = Options()
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+
+driver = get_driver()
+driver.get("http://www.google.com")
+
+st.code(driver.page_source)
